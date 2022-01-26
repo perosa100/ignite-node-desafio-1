b8e25f36779967f597144adf7b09bb3952f2bce2
Patrick Perosa <patrickperosapp@gmail.com>
2022-01-26 15:48:23 -0400

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,109 @@
+const express = require("express");
+const cors = require("cors");
+
+const { v4: uuid } = require("uuid");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+const users = [];
+
+function checksExistsUserAccount(request, response, next) {
+  const { username } = request.headers;
+
+  const user = users.find((user) => user.username === username);
+
+  if (!user) {
+    return response.status(400).json({ error: "Usuario Inexistente" });
+  }
+  request.user = user;
+
+  return next();
+}
+
+app.post("/users", (request, response) => {
+  const { name, username } = request.body;
+
+  const userExists = users.find((user) => user.username === username);
+
+  if (userExists) {
+    return response.status(400).json({ error: "Username ja Existe" });
+  }
+
+  const user = { id: uuid(), name, username, todos: [] };
+
+  users.push(user);
+
+  return response.status(201).json(user);
+});
+
+app.get("/todos", checksExistsUserAccount, (request, response) => {
+  const { user } = request;
+
+  return response.json(user.todos);
+});
+
+app.post("/todos", checksExistsUserAccount, (request, response) => {
+  const { title, deadline } = request.body;
+  const { user } = request;
+
+  const todo = {
+    id: uuid(),
+    title,
+    done: false,
+    deadline: new Date(deadline),
+    created_at: new Date(),
+  };
+  user.todos.push(todo);
+
+  return response.status(201).json(todo);
+});
+
+app.put("/todos/:id", checksExistsUserAccount, (request, response) => {
+  const { title, deadline } = request.body;
+  const { user } = request;
+  const { id } = request.params;
+
+  const todo = user.todos.find((todo) => (todo.id = id));
+
+  if (!todo) {
+    return response.status(404).json({ error: "Tarefa Inexistente" });
+  }
+  todo.title = title;
+  todo.deadline = new Date(deadline);
+
+  return response.json(todo);
+});
+
+app.patch("/todos/:id/done", checksExistsUserAccount, (request, response) => {
+  const { user } = request;
+  const { id } = request.params;
+
+  const todo = user.todos.find((todo) => (todo.id = id));
+
+  if (!todo) {
+    return response.status(404).json({ error: "Tarefa Inexistente" });
+  }
+  todo.done = true;
+
+  return response.json(todo);
+});
+
+app.delete("/todos/:id", checksExistsUserAccount, (request, response) => {
+  const { user } = request;
+  const { id } = request.params;
+
+  const todoIndex = user.todos.findIndex((todo) => (todo.id = id));
+
+  if (todoIndex === -1) {
+    return response.status(404).json({ error: "Tarefa Inexistente" });
+  }
+
+  user.todos.splice(todoIndex, 1);
+
+  return response.status(204).json();
+});
+
+module.exports = app;
